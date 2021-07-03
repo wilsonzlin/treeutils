ede74202232ecdcdb7ae023d92c9b64e2b465f22
Wilson Lin <code@wilsonl.in>
2021-07-03 20:49:19 +1000

    Use bin dir

--- build.js
+++ build.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 
+fs.mkdirSync("bin", { recursive: true });
 for (const f of fs.readdirSync("js")) {
   if (!f.endsWith(".js")) {
     continue;
   }
   fs.writeFileSync(
-    `js/${f}`,
-    `#!/usr/bin/env node\n${fs.readFileSync(`js/${f}`)}`
+    `bin/${f.slice(0, -3)}`,
+    `#!/usr/bin/env node\n${fs.readFileSync(`js/${f}`)}`,
+    { mode: 0o555 }
   );
-  fs.chmodSync(`js/${f}`, 0o555);
 }
